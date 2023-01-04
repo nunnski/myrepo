7986db03b8093e8677ff397cc129dfe878e18156
nunnski <s.nunn@mdx.ac.uk>
2023-01-04 15:23:34 +0000

    R script note

---
+++ wrapping_library_calls.R
@@ -0,0 +1,8 @@
+{
+  library(tidyverse)
+  library(ggplot2)
+}
+
+# this is a test of using curly brackets for wrapping library calls as per a tweet by Tokhir Dadaev.   Benefits are 
+# collapsible within RStudio
+# can be run interactively as one line using Ctrl+Enter
